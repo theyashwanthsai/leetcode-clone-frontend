817eb49d2f31d173d52510be03b6a1edb87f89eb
Sai Yashwanth <saiyashwanth@Sais-MacBook-Air.local>
2023-06-02 14:08:52 +0530

    v1

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -6,7 +6,19 @@ function Problems() {
 
   return (
     <>
-      <p>problems</p>
+      <table className="border-4">
+        <tr className="p-3 ">
+          <th className="p-3 border-4">Title</th>
+          <th className="p-3 border-4">Difficulty</th>
+          <th className="p-3 border-4">Accpetance</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        
+      </table>
         
     </>
   )
