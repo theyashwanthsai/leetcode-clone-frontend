288cec831e5e6fe403c9293c18228bf66194a981
Sai Yashwanth <taddishetty34@gmail.com>
2023-06-02 17:26:43 +0530

    v1

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -1,23 +1,32 @@
 import React from "react"
-
+import problemList from "./Problemset";
 
 function Problems() {
 
 
   return (
     <>
-      <table className="border-4">
+      <table className="border-4 w-50 mx-14">
+        <thead>
+
+        
         <tr className="p-3 ">
           <th className="p-3 border-4">Title</th>
           <th className="p-3 border-4">Difficulty</th>
           <th className="p-3 border-4">Accpetance</th>
         </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+        </thead>
+        <tbody>
+        {problemList.map((problem, index) => (
+            <tr key = {index} className="p-3 ">
+            <td className="p-3 border-4">{problem.title}</td>
+            <td className="p-3 border-4">{problem.difficulty}</td>
+            <td className="p-3 border-4">{problem.acceptance}</td>
+          </tr>
+        )) }
         
+        
+        </tbody>
       </table>
         
     </>
---
+++ src/Components/Problemset.js
@@ -0,0 +1,55 @@
+const problemList = [
+    {
+      title: "Problem 1",
+      difficulty: "Medium",
+      acceptance: "80%",
+    },
+    {
+      title: "Problem 2",
+      difficulty: "Easy",
+      acceptance: "90%",
+    },
+    {
+      title: "Problem 3",
+      difficulty: "Hard",
+      acceptance: "70%",
+    },
+    {
+      title: "Problem 4",
+      difficulty: "Medium",
+      acceptance: "85%",
+    },
+    {
+      title: "Problem 5",
+      difficulty: "Easy",
+      acceptance: "75%",
+    },
+    {
+      title: "Problem 6",
+      difficulty: "Hard",
+      acceptance: "60%",
+    },
+    {
+      title: "Problem 7",
+      difficulty: "Medium",
+      acceptance: "95%",
+    },
+    {
+      title: "Problem 8",
+      difficulty: "Hard",
+      acceptance: "50%",
+    },
+    {
+      title: "Problem 9",
+      difficulty: "Easy",
+      acceptance: "85%",
+    },
+    {
+      title: "Problem 10",
+      difficulty: "Medium",
+      acceptance: "70%",
+    },
+    // Add more problem objects as needed
+  ];
+  
+  export default problemList;
