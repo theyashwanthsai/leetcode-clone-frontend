56069bbbfb1a20bcec1838c4d5fbdbf958968533
Sai Yashwanth <taddishetty34@gmail.com>
2023-06-02 22:19:56 +0530

    routes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,35 @@ function Login() {
 
   return (
     <>
-      <p>login</p>
+      <div>
+        <h1 className="text-center">Login</h1>
+        <div className="text-center border-4 p-3 m-5">
+
+        <div className="p-3">
+        <label htmlFor="email" className="p-3">Email</label>
+        <input type="email"
+        name = "email"
+        placeholder="Your email here" 
+        className="p-3" />
+        </div>
+
+        <div className="p-3">
+          <label htmlFor="password" className="p-3">Password</label>
+          <input type="password" 
+          placeholder="*********"
+          className="p-3"/>
+        </div>
+
+      <div>
+        <button className="border-2">Submit</button>
+      </div>
+
+
+
+
+
+        </div>
+      </div>
         
     </>
   )
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -6,7 +6,35 @@ function Signup() {
 
   return (
     <>
-      <p>signup!</p>
+            <div>
+        <h1 className="text-center">Signup</h1>
+        <div className="text-center border-4 p-3 m-5">
+
+        <div className="p-3">
+        <label htmlFor="email" className="p-3">Email</label>
+        <input type="email"
+        name = "email"
+        placeholder="Your email here" 
+        className="p-3" />
+        </div>
+
+        <div className="p-3">
+          <label htmlFor="password" className="p-3">Password</label>
+          <input type="password" 
+          placeholder="*********"
+          className="p-3"/>
+        </div>
+
+      <div>
+        <button className="border-2">Submit</button>
+      </div>
+
+
+
+
+
+        </div>
+      </div>
         
     </>
   )
